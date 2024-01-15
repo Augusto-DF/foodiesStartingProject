9113009717ebb13cd12b9f668be8e558dd4bcb4a
Augusto-DF <neoaugustocode@gmail.com>
2024-01-15 15:04:49 -0300

    Add loading state to the meals page

---
+++ app/meals/loading-meals.js
@@ -0,0 +1,7 @@
+import styles from "./loading-meals.module.css";
+
+const MealsLoadingPage = () => {
+  return <p className={styles.loading}>Fatching Meals...</p>;
+};
+
+export default MealsLoadingPage;
--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,11 +1,26 @@
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import MealsLoadingPage from "./loading-meals";
 
-const Meals = async () => {
+const MealsData = async () => {
   const meals = await getMeals();
+  console.log("meals", meals);
+  return (
+    <ul>
+      {meals.map((meal) => (
+        <li key={meal.id}>{meal.id}</li>
+      ))}
+    </ul>
+  );
+};
 
+const Meals = () => {
   return (
     <main>
       <h1>Meals</h1>
+      <Suspense fallback={<MealsLoadingPage />}>
+        <MealsData />
+      </Suspense>
     </main>
   );
 };
